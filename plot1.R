b5c0797cc9887ee9ee11d18ca0e2cbb04ea11447
Célio Júnior <celiomsj@gmail.com>
2014-06-08 18:24:33 -0300

    Project resolution.

---
+++ plot1.R
@@ -0,0 +1,33 @@
+##
+# Coursera
+# Exploratory Data Analysis
+# Course Project 1
+# Plot 1
+#
+
+# Load data
+df <- read.table("household_power_consumption.txt", header=T, sep=";",
+                 stringsAsFactors=F, dec=".")
+# Subset for 2007-02-01 and 2007-02-02 only
+df <- df[df$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# Convert variables
+df$Date <- strptime(df$Date,"%d/%m/%Y")
+df$Time <- strptime(paste(df$Date, df$Time), "%F %T")
+df$Global_active_power <- as.numeric(df$Global_active_power)
+df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
+df$Voltage <- as.numeric(df$Voltage)
+df$Global_intensity <- as.numeric(df$Global_intensity)
+df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
+df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
+df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
+
+# Set locale to format weekdays in english
+Sys.setlocale("LC_TIME", "C")
+
+# Plots
+png(filename="plot1.png", width=480, height=480)
+hist(df$Global_active_power, col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,46 @@
+##
+# Coursera
+# Exploratory Data Analysis
+# Course Project 1
+# Plot 4
+#
+
+# Load data
+df <- read.table("household_power_consumption.txt", header=T, sep=";",
+                 stringsAsFactors=F, dec=".")
+# Subset for 2007-02-01 and 2007-02-02 only
+df <- df[df$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# Convert variables
+df$Date <- strptime(df$Date,"%d/%m/%Y")
+df$Time <- strptime(paste(df$Date, df$Time), "%F %T")
+df$Global_active_power <- as.numeric(df$Global_active_power)
+df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
+df$Voltage <- as.numeric(df$Voltage)
+df$Global_intensity <- as.numeric(df$Global_intensity)
+df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
+df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
+df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
+
+# Set locale to format weekdays in english
+Sys.setlocale("LC_TIME", "C")
+
+# Plots
+png(filename="plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(df$Time, df$Global_active_power, type="l",
+     xlab="", ylab="Global Active Power")
+
+plot(df$Time, df$Voltage, type="l",
+     xlab="datetime", ylab="Voltage")
+
+plot(df$Time, df$Sub_metering_1, type="l", col="black",
+     xlab="", ylab="Energy sub metering")
+lines(df$Time, df$Sub_metering_2, col="red")
+lines(df$Time, df$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lty=1, bty="n")
+
+plot(df$Time, df$Global_reactive_power, type="l",
+     xlab="datetime", ylab="Global_reactive_power")
+dev.off()
